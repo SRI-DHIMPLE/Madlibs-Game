0f1f817a1a5a6e72769bd60c6f6988fdaa6d59cd
SRI-DHIMPLE <sridhimple3o@gmail.com>
2024-09-24 16:24:18 -0500

    code changes

---
+++ madlib-code.py
@@ -0,0 +1,26 @@
+#creating a mad libs game
+print("STORY - The Great Escape")
+Adjective_1 = input("Adjective 1: ")
+Adjective_2 = input("Adjective 2: ")
+Animal = input("Animal: ")
+Name = input("Name: ")
+Place = input("Place: ")
+Verb_ending_in_ing = input("Verb ending in ing: ")
+Noun_1 = input("Noun: ")
+Adjective_3 = input("Adjective 3: ")
+Noun_2 = input("Noun: ")
+Plural_noun = input("Plural noun: ")
+Adjective_4 = input("Adjective 4: ")
+Creature = input("Creature: ")
+Adjective_5 = input("Adjective 5: ")
+Verb = input("Verb: ")
+Verb_2 = input("Verb: ")
+Emotion = input("Emotion: ")
+Adjective_6 = input("Adjective 6: ")
+
+story = (f"In a {Adjective_1} castle, there lived a {Adjective_2} {Animal} named {Name}. Every day, {Name} would dream of escaping to the {Place}. One day, while {Verb_ending_in_ing} near the {Noun_1}, {Name} found a {Adjective_3} {Noun_2} that would help in the great escape! "
+         f"{Name} gathered all of their {Plural_noun} and set off on the adventure. As they journeyed, they encountered a {Adjective_4} {Creature} that wanted to join them. Together, they decided to {Verb} through the {Place} filled with {Plural_noun}."
+         f"After a long journey, they reached the {Adjective_5} gate of the {Place}. With a {Noun_1} in hand, {Name} yelled, Let's {Verb} our way to freedom!" 
+         f"They all {Verb_2} through the gate, feeling {Emotion}."
+         f"Finally, they arrived at the {Adjective_6} {Place} where they could be free to {Verb} and {Verb} all day long. {Name} looked at their new friends and said, This is the best day ever!")
+print(story)
